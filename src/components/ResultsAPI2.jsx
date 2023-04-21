8a58c99910120d6452ca770a17a8ab2fccf0892e
Meint Local <mwverhoeff+vscode@gmail.com>
2023-04-21 11:10:20 +0200

    working on pushing data

--- src/components/ResultsAPI2.jsx
+++ src/components/ResultsAPI2.jsx
@@ -7,7 +7,7 @@ import { load } from "cheerio";
 
 const baseURL = 'https://www.mijnwoordenboek.nl/puzzelwoordenboek/'
 const selector2 = 'body > div.main-holder > div > div > div > div > div > div.span8.right > div > div:nth-child(n) > table > tbody > tr:nth-child(n) > td > div:nth-child(n)'
-const kvURL = 'https://worker-kv-api.0nu2sngw3778.workers.dev/set?' //searchword=getij'
+const kvURL = 'https://worker-kv-api.0nu2sngw3778.workers.dev/' //set?searchword=getij'
 
 export default function Results(props) {
     const { searchword, solution, setSolution } = props
@@ -40,13 +40,60 @@ export default function Results(props) {
     React.useEffect(() => {
         /*reset solution result after searchword has been updated*/
         setSolution([])
+        let foundInCache = false
         /*issue the API call skip when searchword is not set (1st run)*/
-        searchword && Axios.get(`${kvURL}searchword=${searchword}`)
-        .then(response => {
-            console.log(`Api call to: ${kvURL}searchword=${searchword}`)
-            const $ = load(response.data)
-            console.log($)
-        })
+        async function makeApiCalls() {
+            try {
+                console.log(`Check Cache`)
+                let response = await Axios.get(`${kvURL}set?searchword=${searchword}`)
+                if (!response.data.found) {
+                    console.log('Not in Cache - Checking Web')
+                    response = searchword && await Axios.get(`${baseURL}${searchword}\/1\/1`)
+                    const $ = load(response.data)
+                    $(selector2).toArray().map((item,i) => appendOrUpdateWoord(i, $(item).text(),solution))
+                    console.log('Update Cache')
+                    response = await Axios.post(kvURL,`{searchword:${searchword}, woorden:${solution}`)
+                    console.log(response.data)
+                } else {
+                    console.log('Found in Cache - setSolution')
+                    response.data.woorden.map((item,i) => appendOrUpdateWoord(i,item,solution))
+                }
+
+            } catch (error) {
+                console.log({error})
+            }
+        }
+        searchword && makeApiCalls()  // dont make api calls with empty searchword e.g. when app loads first time.
+    }, [searchword]);
+
+    //     const checkInCache = async () => {
+    //         console.log(`check CF`)
+    //             const response = await Axios.get(`${kvURL}searchword=${searchword}`)
+    //             console.log(`Api call to: ${kvURL}searchword=${searchword}`)
+    //             console.log(response.data)
+    //             if (response.data.found) 
+    //             {
+    //                 response.data.woorden.map((item,i) => appendOrUpdateWoord(i,item,solution))
+    //                 foundInCache = true
+    //                 console.log('CF-Cache')
+    //             }
+    //             true
+    //         }
+    //             const checkOnWeb = async () => {
+    //                 console.log(`Web-Call : ${searchword}`)
+    //                 const response = searchword && await Axios.get(`${baseURL}${searchword}\/1\/1`)
+    //                 console.log(`API call to: ${baseURL}${searchword}\/1\/1`) 
+    //                 const $ = load(response.data)
+    //                 $(selector2).toArray().map((item,i) => appendOrUpdateWoord(i, $(item).text(),solution))
+    //             }
+    //         checkInCache()
+    //         foundInCache && checkOnWeb()
+    // }, [searchword]);
+                 
+            
+            //console.log(response.data)
+            // console.log($)
+        //})
         // .catch(error => console.log(error))
         // searchword && Axios.get(`${baseURL}${searchword}\/1\/1`)
         //     .then(response => {
@@ -55,30 +102,30 @@ export default function Results(props) {
         //         $(selector2).toArray().map((item,i) => appendOrUpdateWoord(i, $(item).text(),solution))
         //     })
         //     .catch(error => console.log(error))
-    }, [searchword]);
+    //}, [searchword]);
 
     // console.log(`searchSolution: ${JSON.stringify(solution)}`)
     // console.log(`rijen ${solution.length}`)
-    // const results = solution.map((e) => (
-    //     <div key={e.letters}>
-    //         <p className="letters">{e.letters} - letters</p>
-    //         <p className="results">
-    //             {e.woorden.reduce((acc, word, index) => {
-    //                 acc.push(
-    //                     <span key={word} className="result"> • {word} </span>
-    //                 );
-    //                 if (index === e.woorden.length - 1) {
-    //                     acc.push(<span key={`separator-${index}`}>•</span>);
-    //                 }
-    //                 return acc;
-    //             }, [])}</p>
-    //     </div>
-    // ));
+    const results = solution.map((e) => (
+        <div key={e.letters}>
+            <p className="letters">{e.letters} - letters</p>
+            <p className="results">
+                {e.woorden.reduce((acc, word, index) => {
+                    acc.push(
+                        <span key={word} className="result"> • {word} </span>
+                    );
+                    if (index === e.woorden.length - 1) {
+                        acc.push(<span key={`separator-${index}`}>•</span>);
+                    }
+                    return acc;
+                }, [])}</p>
+        </div>
+    ));
     
     return (
         <div className="results--form">
             {/* {searchword} */}
-            {/* {results} */}
+            {results}
         </div>
     )
 }
